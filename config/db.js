da54a2d5a84aa84e8edf4012da41e28c92c9d0bf
PhatLeTien <tienphataki@gmail.com>
2026-04-03 10:35:13 +0700

    fix db crash

--- config/db.js
+++ config/db.js
@@ -1,15 +1,26 @@
 const mysql = require("mysql2");
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123",
+//   database: "todo_app",
+// });
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123",
-  database: "todo_app",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
+// ❗ KHÔNG throw error (tránh crash)
 db.connect(err => {
-  if (err) throw err; 
-  console.log("MySQL Connected"); 
+  if (err) {
+    console.log("DB connection error:", err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
 module.exports = db;
